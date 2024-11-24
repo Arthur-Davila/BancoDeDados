4b9546a459ba3b42f3a387055a4afd97238f8e14
arthcc <arthcc@outlook.com>
2024-11-24 12:31:42 -0300

    Inicializando o banco de dados

---
+++ SolennitaStellare/src/main/java/com/example/grupo/SolennitaStellare/SolennitaStellareApplication.java
@@ -0,0 +1,13 @@
+package com.example.grupo.SolennitaStellare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolennitaStellareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolennitaStellareApplication.class, args);
+	}
+
+}
---
+++ SolennitaStellare/src/test/java/com/example/grupo/SolennitaStellare/SolennitaStellareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.grupo.SolennitaStellare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolennitaStellareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
