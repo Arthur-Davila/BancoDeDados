0221204bc74b84f083e9e7ff66120faf8e3b0895
arthcc <arthcc@outlook.com>
2024-11-24 13:14:20 -0300

    Testando versão 3.3.6

--- SolennitaStellare/src/test/java/com/grupo/SolennitaStellare/SolennitaStellareApplicationTests.java
+++ SolennitaStellare/src/test/java/com/grupo/SolennitaStellare/SolennitaStellareApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.grupo.SolennitaStellare;
+package com.grupo.SolennitaStellare;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
